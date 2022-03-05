674c13c36ce83fe3644a431e1a4ebe56e803f4f0
anton156 <aviskic@gmail.com>
2022-03-05 18:12:28 +0100

    Add project files.

---
+++ PraksaProjektBackend/Auth/RegisterModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PraksaProjektBackend.Auth
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        public string? Firstname { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        public string? Lastname { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Adress is required")]
+        public string? Address { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string? Password { get; set; }
+    }
+}
---
+++ PraksaProjektBackend/Auth/UserRoles.cs
@@ -0,0 +1,9 @@
+﻿namespace PraksaProjektBackend.Auth
+{
+    public static class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string Customer = "Customer";
+        public const string Organizer = "Organizer";
+    }
+}
