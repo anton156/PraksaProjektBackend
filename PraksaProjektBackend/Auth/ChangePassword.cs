8895668161c6204a0ff4e0c76ba0f215c830a4b0
ana bilos <ana.bilos@student.fsre.ba>
2022-03-09 18:18:14 +0100

    Added Change user password

---
+++ PraksaProjektBackend/Auth/ChangePassword.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PraksaProjektBackend.Auth
+{
+    public class ChangePassword
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Current password is required")]
+        public string CurrentPassword { get; set; }
+
+        [Required(ErrorMessage = "New password is required")]
+        public string NewPassword { get; set; }
+
+        [Required(ErrorMessage = "ConfirmNewPassword is required")]
+        public string ConfirmNewPassword { get; set; }
+    }
+}
