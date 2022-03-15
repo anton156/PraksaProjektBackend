cd3d0fc158fc38c8c6a1a0296b43e9732801cfd6
ana bilos <ana.bilos@student.fsre.ba>
2022-03-15 14:12:42 +0100

    Added function for getCurrentUser

--- PraksaProjektBackend/Models/Event.cs
+++ PraksaProjektBackend/Models/Event.cs
@@ -1,10 +1,16 @@
-﻿namespace PraksaProjektBackend.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PraksaProjektBackend.Models
 {
     public class Event
     {
         public int EventId { get; set; }
 
         public string? EventName { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        [DataType(DataType.Date)]
+        public DateOnly? EventDate { get; set; }
 
         public string? Content { get; set; }
 
