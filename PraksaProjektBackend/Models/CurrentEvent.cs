9749cfa854052bb6bf5662db8b4ba83fadf573f3
anton156 <aviskic@gmail.com>
2022-03-15 13:34:32 +0100

    Added model for currentevent and eventype

---
+++ PraksaProjektBackend/Models/CurrentEvent.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PraksaProjektBackend.Models
+{
+    public class CurrentEvent
+    {
+        public int CurrentEventId { get; set; }
+
+        public string? EventName { get; set; }
+
+        public string? Content { get; set; }
+
+        public string? OrganizersName { get; set; }
+
+        public float Price { get; set; }
+
+        public int NumberOfSeats { get; set; }
+
+        [NotMapped]
+        public IFormFile? EventImage { get; set; }
+
+        public string? ImagePath { get; set; }
+
+        public DateTime Begin { get; set; }
+
+        public DateTime End { get; set; }
+
+        public EventType? EventType { get; set; }
+
+        public int EventTypeId { get; set; }
+
+        public Venue? Venue { get; set; }
+
+        public int VenueId { get; set; }
+    }
+}
---
+++ PraksaProjektBackend/Models/EventType.cs
@@ -0,0 +1,11 @@
+﻿namespace PraksaProjektBackend.Models
+{
+    public class EventType
+    {
+        public int EventTypeId { get; set; }
+
+        public string? EventTypeName { get; set; }
+
+        public List<CurrentEvent>? CurrentEvents { get; set; }
+    }
+}
